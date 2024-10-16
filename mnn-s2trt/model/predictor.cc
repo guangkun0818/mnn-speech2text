9ed61b04f3f5d32169e7c65a37dc0ffbcf8c14e2
guangkun0818 <609946862@qq.com>
2024-10-16 21:59:50 +0800

    [predictor-setup] - setting up mnn stateless predictor for transducer.

---
+++ mnn-s2trt/model/predictor.cc
@@ -0,0 +1,40 @@
+// Author: guangkun0818
+// Email: 609946862@qq.com
+// Created on 2023.09.30
+// Wrapped Predictor of transducer of mnn runtime.
+
+#include "mnn-s2trt/model/predictor.h"
+
+namespace s2trt {
+namespace models {
+
+MnnPredictor::MnnPredictor(const char* predictor_model, size_t context_size)
+    : context_size_(context_size) {
+  // TODO(guangkun0818): modify to support more config.
+  config_.numThread = 8;
+  config_.type = MNNForwardType::MNN_FORWARD_CPU;
+
+  this->model_ = std::shared_ptr<mnn::Interpreter>(
+      mnn::Interpreter::createFromFile(predictor_model));
+  CHECK_NE(this->model_, nullptr);
+
+  this->session_ = model_->createSession(config_);
+  CHECK_NE(this->session_, nullptr);
+}
+
+void MnnPredictor::Init(const int beam_size) {
+  // Predictor state shape: {beam_size, context_size - 1}
+  std::vector<int> init_input_shape = {beam_size, this->context_size_ - 1};
+
+  pred_state_ = std::shared_ptr<mnn::Tensor>(
+      mnn::Tensor::create<float>(init_input_shape, NULL, MNN::Tensor::CAFFE));
+  // Init predictor state as 0.0
+  for (int i = 0; i < pred_state_->elementSize(); i++) {
+    pred_state_->host<float>()[i] = 0.0;
+  }
+}
+
+void MnnPredictor::StreamingStep(const std::vector<int>& pred_in) {}
+
+}  // namespace models
+}  // namespace s2trt
