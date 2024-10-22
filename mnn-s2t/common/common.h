5524eadc50293a40ba09d53745df639a973b47c7
guangkun0818 <609946862@qq.com>
2024-10-23 01:35:13 +0800

    [greedy-decoding] - minor fix when apply ctest.

--- mnn-s2t/common/common.h
+++ mnn-s2t/common/common.h
@@ -2,8 +2,8 @@
 // Email: 609946862@qq.com
 // Created on 2023.08.29
 
-#ifndef _MNN_S2TRT_COMMON_H_
-#define _MNN_S2TRT_COMMON_H_
+#ifndef _MNN_S2T_COMMON_H_
+#define _MNN_S2T_COMMON_H_
 
 #include "MNN/Interpreter.hpp"
 #include "MNN/MNNForwardType.h"
